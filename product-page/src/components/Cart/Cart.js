f7deac42ab404cb497ad9c171116f442909ab3a5
Emanuele Braco <emanuele.braco@gmail.com>
2022-12-02 18:57:44 +0100

    implement logic for cart

---
+++ product-page/src/components/Cart/Cart.js
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import { Cartcontext } from "../../context/Context";
+
+const Cart = () => {
+  const Globalstate = useContext(Cartcontext);
+  const state = Globalstate.state;
+  const dispatch = Globalstate.dispatch;
+
+  const total = state.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+  return (
+    <div className="cart">
+      {state.map((item, index) => {
+        return (
+          <div className="card" key={index}>
+            <img src={item.image} style={{ width: "50px" }} alt="" />
+            <p>{item.title}</p>
+            <p>{item.quantity * item.price}</p>
+            <button>Checkout</button>
+            <h2 onClick={() => dispatch({ type: "REMOVE", payload: item })}>
+              x
+            </h2>
+          </div>
+        );
+      })}
+      {state.length > 0 && (
+        <div className="total">
+          <h2>{total}</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
